82c1f37afea8c78f68328fd90181c9f01f6d61cb
Vijay Anand R <vanandr@cisco.com>
2015-10-19 05:37:35 +0530

    Iterator destroy
    Signed-off-by: Vijay Anand R <vanandr@cisco.com>

--- snbiFe/impl/an_if_linux.c
+++ snbiFe/impl/an_if_linux.c
@@ -198,6 +198,7 @@ an_if_enable_nd_on_all_intfs (void)
             OLIBC_RETVAL_SUCCESS) {
         an_if_enable_nd_on_intf(if_linux_info);
     }
+    olibc_list_iterator_destroy(&if_list_iter);
     return;
 }
 
@@ -217,6 +218,7 @@ an_if_walk (an_if_walk_func func, void *data)
             OLIBC_RETVAL_SUCCESS) {
         func(if_linux_info->if_index, data);
     }
+    olibc_list_iterator_destroy(&if_list_iter);
     return;
 }
 
