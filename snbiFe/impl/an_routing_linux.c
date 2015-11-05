22d51f18532940f3761fe5d4792bfa09c6c7d95e
Vijay Anand R <vanandr@cisco.com>
2015-11-05 14:33:50 +0530

    Tunnel flap work around
    Signed-off-by: Vijay Anand R <vanandr@cisco.com>

--- snbiFe/impl/an_routing_linux.c
+++ snbiFe/impl/an_routing_linux.c
@@ -81,7 +81,7 @@ void an_notify_routing_device_id_availble()
             "--dao-if-filter snbi-* --ignore-pio "
             "--dao-addr-filter fd08::/16 --dag-if-filter snbi_tun_* "
             "-p fd08::/16 --syslog --stderr --verbose "
-            "--rank %s > /tmp/rpl.log 2>&1 &", rank);
+            "--rank %s --interval 40000 > /tmp/rpl.log 2>&1 &", rank);
     DEBUG_AN_LOG(AN_LOG_BS_EVENT, AN_DEBUG_MODERATE, NULL,
             "\n%sRPL Execs: \n\t%s \n\t%s", an_bs_event,
             "sysctl -w net.ipv6.conf.all.forwarding=1", cmd);
